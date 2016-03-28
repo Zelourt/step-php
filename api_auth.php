92b15b4ac9a090ce04b9c89652ec40c6e103ecf9
Zelourt Rien <Zelourt@gmail.com>
2016-03-28 19:43:57 +0300

    1

---
+++ api_auth.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zelourt
+ * Date: 28.03.16
+ * Time: 19:42
+ */
+
+
+if (isset($_POST['submit']) AND $_POST['submit'] == "logIn"){
+    echo $_POST['login'];
+}
